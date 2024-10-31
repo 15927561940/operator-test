01bcfbdd9a90ddf101b83e19846d6ecaad943482
bobo <bobo>
2024-10-31 22:36:38 +0800

    zk

--- api/v1/zookeepercluster_types.go
+++ api/v1/zookeepercluster_types.go
@@ -29,7 +29,10 @@ type ZooKeeperClusterSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of ZooKeeperCluster. Edit zookeepercluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Foo      string `json:"foo,omitempty"`
+	Replicas *int32 `json:"replicas,omitempty"`
+	Image    string `json:"image,omitempty"`
+	Version  string `json:"version,omitempty"`
 }
 
 // ZooKeeperClusterStatus defines the observed state of ZooKeeperCluster
